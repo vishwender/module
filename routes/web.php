f018558e3b4bbc5ed63830b7ca8c6cc0ab292d42
vishu <vishu@vishus-MacBook-Air.local>
2020-08-19 16:21:35 +0530

    changes to login

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// Route::get('/', function () {
+//     return view('welcome');
+// })->middleware('auth');
+Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
